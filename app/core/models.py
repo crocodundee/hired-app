3956794f771963a556ad1b01cce554b6a36717af
Anastasiia Didan <anastasia.didan.asm@gmail.com>
2020-07-20 20:29:28 +0300

    Created skill model to describe applicant skills level

--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,24 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
+class Skill(models.Model):
+    """Applicant skill item"""
+
+    name = models.CharField(max_length=255, blank=False)
+    level = models.PositiveSmallIntegerField(max_value=10, blank=False)
+
+    def __str__(self):
+        """String representing of skill model"""
+        return self.name
+
+
 class Resume(models.Model):
     """Basic resume for job applicant"""
 
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     position = models.CharField(max_length=255, blank=False)
     summury = models.TextField(blank=False)
+    skills = models.ManyToManyField(to="Skill")
 
     def __str__(self):
         """String represent of model"""
