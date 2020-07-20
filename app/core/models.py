4e8f65029f71a988317f562b69ddc433b66a41c9
Anastasiia Didan <anastasia.didan.asm@gmail.com>
2020-07-20 20:17:13 +0300

    Created simple resume model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Resume(models.Model):
+    """Basic resume for job applicant"""
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    position = models.CharField(max_length=255, blank=False)
+    summury = models.TextField(blank=False)
+
+    def __str__(self):
+        """String represent of model"""
+        return f"{self.user.first_name} {self.user.last_name} - {self.position}"
