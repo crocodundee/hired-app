3a0f676774c16b1b7b0acf8722e712205799ed11
Anastasiia Didan <anastasia.didan.asm@gmail.com>
2020-07-25 13:54:54 +0300

    User authentication views

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
 
-# Create your views here.
+
+class UserRegisterView(FormView):
+    """Register users view"""
+
+    form_class = UserCreationForm
+    success_url = "/"
+    template_name = "/sign-up.html"
+
+
+class UserLoginView(LoginView):
+    """User login view"""
+
+    template_name = "/login.html"
+
+
+class UserLogoutView(LogoutView):
+    """User logout then login view"""
+
+    template_name = "/logout.html"
+    next_page = "/login"
