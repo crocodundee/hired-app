9199fe6767d715851b65ddf9c56fad1b0d0fb1b4
Anastasiia Didan <anastasia.didan.asm@gmail.com>
2020-07-25 14:32:13 +0300

    Base templates structure

--- app/user/views.py
+++ app/user/views.py
@@ -8,17 +8,17 @@ class UserRegisterView(FormView):
 
     form_class = UserCreationForm
     success_url = "/"
-    template_name = "/sign-up.html"
+    template_name = "user/sign-up.html"
 
 
 class UserLoginView(LoginView):
     """User login view"""
 
-    template_name = "/login.html"
+    template_name = "user/login.html"
 
 
 class UserLogoutView(LogoutView):
     """User logout then login view"""
 
     template_name = "/logout.html"
-    next_page = "/login"
+    next_page = "user/login"
